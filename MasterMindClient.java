2b414068c41918dfe657f775cb4effa4bb7ac196
Quimberton <44734859+Quimberton@users.noreply.github.com>
2018-11-04 03:37:27 -0500

    Add files via upload

---
+++ MasterMindClient.java
@@ -0,0 +1,12 @@
+package Mastemind;
+
+public class MasterMindClient {
+	
+	
+	public static void main(String[]args) {
+		int num = 5;
+		Mastermind game = new Mastermind(num);
+		System.out.println("take a guess there are "+num+" numbers");
+		game.play();
+	}
+}
