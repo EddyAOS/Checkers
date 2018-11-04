2665cdb4a66bd741b4e54099c7d46d8d19cf7fa2
EddyAOS <38820267+EddyAOS@users.noreply.github.com>
2018-11-04 07:53:40 -0500

    Add files via upload

--- MasterMindClient.java
+++ MasterMindClient.java
@@ -1,12 +1,14 @@
-package Mastemind;
 
 public class MasterMindClient {
-	
-	
-	public static void main(String[]args) {
-		int num = 5;
-		Mastermind game = new Mastermind(num);
-		System.out.println("take a guess there are "+num+" numbers");
-		game.play();
-	}
+
+
+    public static void main(String[]args) {
+        int num = 5;
+        Mastermind game = new Mastermind(num);
+        System.out.println("take a guess there are "+num+" numbers");
+      //  game.play();
+        MastermindSolver solver = new MastermindSolver(game);
+        solver.solution();
+
+    }
 }
